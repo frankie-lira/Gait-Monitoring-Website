2eca0fe0b125d40deb13183186b0921160bbf9b6
Melody Hu <melodyhu2003@gmail.com>
2021-07-07 00:31:49 -0700

    updated graph

--- plotly.js
+++ plotly.js
@@ -1,61 +1,31 @@
-// reference: https://plotly.com/javascript/time-series/
-
-d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv", function(err, rows){
-
-  function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
+function rand() {
+  return Math.random();
 }
 
+var time = new Date();
 
-var sensor1 = {
-  type: "scatter",
-  mode: "lines",
-  name: 'Sensor 1',
-  x: unpack(rows, 'Date'),
-  y: unpack(rows, 'AAPL.High'),
-  line: {color: '#17BECF'}
-}
+var data = [{
+  x: [time],
+  y: [rand()],
+  mode: 'lines',
+  line: {color: '#80CAF6'}
+}]
 
-var sensor2 = {
-  type: "scatter",
-  mode: "lines",
-  name: 'Sensor 2',
-  x: unpack(rows, 'Date'),
-  y: unpack(rows, 'AAPL.Low'),
-  line: {color: '#7F7F7F'}
-}
 
-var data = [sensor1,sensor2];
+Plotly.newPlot('myDiv', data);
 
-var layout = {
-  title: 'Data Visualization',
-  xaxis: {
-    autorange: true,
-    range: ['2015-02-17', '2017-02-16'],
-    rangeselector: {buttons: [
-        {
-          count: 1,
-          label: '1m',
-          step: 'month',
-          stepmode: 'backward'
-        },
-        {
-          count: 6,
-          label: '6m',
-          step: 'month',
-          stepmode: 'backward'
-        },
-        {step: 'all'}
-      ]},
-    //rangeslider: {range: ['2015-02-17', '2017-02-16']},
-    //type: 'date'
-  },
-  yaxis: {
-    autorange: true,
-    range: [86.8700008333, 138.870004167],
-    type: 'linear'
+var cnt = 0;
+
+var interval = setInterval(function() {
+
+  var time = new Date();
+
+  var update = {
+  x:  [[time]],
+  y: [[rand()]]
   }
-};
 
-Plotly.newPlot('myDiv', data, layout);
-})
+  Plotly.extendTraces('myDiv', update, [0])
+
+  if(++cnt === 100) clearInterval(interval);
+}, 1000);
