37378a740ae3189d23677802214e9f0a05012568
Melody Hu <40373356+shinyumh@users.noreply.github.com>
2021-08-19 17:38:40 -0700

    Update Arduino.ino

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -2,7 +2,8 @@
 
 void setup() {
   analogReadResolution(12); // Set analog input resolution to max, 12-bits
-  SerialUSB.begin(9600);
+  SerialUSB.begin(115200);
+  SerialUSB.flush();
 }
 
 void loop() {
@@ -10,5 +11,4 @@ void loop() {
   // 12-bit ADC value to a voltage between 0 and 3.3.
   float voltage = analogRead(A1) * 3.3 / 4096.0; //crossing the 3.3 and 4096.8 will show first data
   SerialUSB.println(voltage); // Print the voltage.
-  delay(1); // Delay 1ms
 }
